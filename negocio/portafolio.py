e928c942b29c57cfa183283aec6ec1c015fd2bff
Marcelo Portillo <mportillo79@gmail.com>
2024-10-15 01:47:16 -0300

    organizando

--- negocio/portafolio.py
+++ negocio/portafolio.py
@@ -7,43 +7,21 @@ class Portafolio:
         self.__id_portafolio = id_portafolio
         self.__saldo_actual = saldo_actual
         self.__fecha_inicio = fecha_inicio
-        
+
     def get_saldo_actual(self):
-        return self.__saldo_actual;
-    
+        return self.__saldo_actual
+
     def set_saldo_actual(self, saldo_actual):
-        self.__saldo_actual =saldo_actual;
-    
+        self.__saldo_actual = saldo_actual
+
     def get_fecha_inicio(self):
-        return self.__fecha_inicio;
-    
+        return self.__fecha_inicio
+
     def set_fecha_inicio(self, fecha_inicio):
-        self.__fecha_inicio =fecha_inicio;
-        
+        self.__fecha_inicio = fecha_inicio
+
     def get_id_portafolio(self):
-        return self.__id_portafolio;
-    
+        return self.__id_portafolio
+
     def set_id_portafolio(self, id_portafolio):
-        self.__id_portafolio =id_portafolio;
-        
-
-    def asignar_saldo_inicial(self, idporfolio):
-        if connection:
-            try:
-                cursor = connection.cursor()
-
-                sql = "update portafolio set saldo_actual = %s where id_portafolio= %s"
-                values = (1000000, self.__id_portafolio)
-                cursor.execute(sql, valores)
-
-                connection.commit()
-
-                cursor.close()
-                connection.close()
-                return True
-
-            except Exception as e:
-                # print(f"Error:{e}")
-                return False
-        else:
-            return "No se pudo conectar a la db"
+        self.__id_portafolio = id_portafolio
--- negocio/servicioReglasNegocio.py
+++ negocio/servicioReglasNegocio.py
@@ -1,4 +1,6 @@
 import bcrypt
+
+
 def crear_encriptacion_password(password):
     salt = bcrypt.gensalt()
     print(salt)
@@ -6,5 +8,28 @@ def crear_encriptacion_password(password):
     print(hashed)
     return hashed
 
-def validar_password(password_has,pas):
-    return bcrypt.checkpw(pas.encode('utf-8'),password_has)
+
+def validar_password(password_has, pas):
+    return bcrypt.checkpw(pas.encode('utf-8'), password_has)
+
+
+def asignar_saldo_inicial(self, idporfolio):
+    if connection:
+        try:
+            cursor = connection.cursor()
+
+            sql = "update portafolio set saldo_actual = %s where id_portafolio= %s"
+            values = (1000000, self.__id_portafolio)
+            cursor.execute(sql, valores)
+
+            connection.commit()
+
+            cursor.close()
+            connection.close()
+            return True
+
+        except Exception as e:
+            # print(f"Error:{e}")
+            return False
+    else:
+        return "No se pudo conectar a la db"
