801b08e442a937bcd05d8e01a5f0e2fc3e7aecf6
Marcelo Portillo <mportillo79@gmail.com>
2024-10-15 18:50:51 -0300

    fix2

--- main.py
+++ main.py
@@ -3,19 +3,8 @@ from negocio import servicioReglasNegocio
 
 
 def main():
-<<<<<<< HEAD
-    pas = servicioReglasNegocio.crear_encriptacion_password("Pepito")
-    print(pas)
-    print(servicioReglasNegocio.validar_password(pas, "Pepito"))
     controllerInicioSesion.inicio_sesion()
 
-    # Carga de saldo inicial y muestra de mensaje
-    print(servicioReglasNegocio.asignar_saldo_inicial(idporfolio=1))
-
-=======
-   controllerInicioSesion.inicio_sesion()
-   
->>>>>>> origin/back
 
 if __name__ == "__main__":
     main()
