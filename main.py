299f15de2db2b5b0ddbb4539a59ff9699cbaa369
Marcelo Portillo <mportillo79@gmail.com>
2024-10-15 19:03:01 -0300

    se comenta cargar saldo

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from controllers import controllerInicioSesion
-from negocio import servicioReglasNegocio
+# from negocio import servicioReglasNegocio
 
 
 def main():
+    # Carga de saldo inicial y muestra de mensaje
+    # print(servicioReglasNegocio.asignar_saldo_inicial(idporfolio=1))
     controllerInicioSesion.inicio_sesion()
 
 
