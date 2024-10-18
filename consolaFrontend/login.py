05361988b4b4ad935a2a2f90eff416e6bb057689
Gustavo Olmos <programacinwebolmos@gmail.com>
2024-10-18 06:04:20 -0300

    se termina login

--- consolaFrontend/login.py
+++ consolaFrontend/login.py
@@ -1,5 +1,6 @@
-from controllers import controllerMenuPrincipal, controllerRegistrarInversor
-
+from controllers import controllerMenuPrincipal, controllerRegistrarInversor, controllerInicioSesion
+from negocio import usuario
+from backend.clasesDAO.usuario_dao import Usuario_Dao
 import getpass
 import os
 
@@ -20,9 +21,16 @@ def mostar_menu_inicio_sesion():
         menu_inicio_sesion()
         opcion = int(input(f"Seleccione una opción (1-5): "))
 
-        if opcion == 1:
-            print(f"Ingresar Usuario y Contraseña")
-            controllerMenuPrincipal.menu_principal() # aca va mi login!!!
+        if opcion == 1: # Gus
+            user = menu_inicio_login()
+
+            """   Seguir con la validación de la contraseña despues!!!     
+            aux = controllerInicioSesion.obtener_usuario(user.get_id_user())
+            print(user)
+            print(aux)
+            validar_ingreso(aux, user.get_password())
+            """
+
         elif opcion == 2:
             print(f"Nuevo Usuario.")
             controllerRegistrarInversor.cargar_menu_registro_inversor()
@@ -38,8 +46,63 @@ def mostar_menu_inicio_sesion():
 
 
 # Gustavo 
-def verify_password(email, password):
-    pass
+"""
+def menu_ingreso():
+    print(f"*******INICIO SESIÓN*******")
+    email = input(f'Ingrese su correo: ')
+    password = input(f'Ingrese su contraseña: ')
+    user = usuario.Usuario(email, password, None)
+    
+# Verifica si los datos existen en la base de datos
+    user = controllerInicioSesion.obtener_usuario(email)
+    
+    if user:
+        # Verifica si la contraseña es correcta
+        if controllerInicioSesion.validar_password(user, password):
+            print("Ingreso correcto")
+            return user
+        else:
+            print("Contraseña incorrecta")
+            return None
+    else:
+        print("Correo electrónico no encontrado")
+        return None
+    """
+def menu_inicio_login():
+    print(f"*******INICIO SESIÓN*******")
+    email = input(f'Ingrese su correo: ')
+    password = input(f'Ingrese su contraseña: ')
+    user = usuario.Usuario(email, password, None)
 
-def login():
-    pass
+    # Verifica si los datos existen en la base de datos
+    usuario_dao = Usuario_Dao()
+    user = usuario_dao.get(email)
+
+    if user:
+        # Verifica si la contraseña es correcta
+        if user.get_password() == password:
+            print("Ingreso correcto")
+            return user
+        else:
+            print("Contraseña incorrecta")
+            return None
+    else:
+        print("Correo electrónico no encontrado")
+        return None
+    
+
+# Seguir con la validación de la contraseña despues!!!
+"""
+def validar_ingreso(user, password):
+    #if user:
+        print(controllerInicioSesion.validar_password(user, password))
+
+        if controllerInicioSesion.validar_password(user, password):
+            print("Ingreso correcto")
+        else:
+            print("Ingreso incorrecto")
+    else:  
+        print("Ingreso incorrecto")
+"""
+
+
--- controllers/controllerInicioSesion.py
+++ controllers/controllerInicioSesion.py
@@ -1,5 +1,6 @@
 from consolaFrontend import login
 from backend.clasesDAO import usuario_dao
+from negocio import servicioReglasNegocio
 
 def inicio_sesion():
     login.mostar_menu_inicio_sesion()
@@ -7,4 +8,14 @@ def inicio_sesion():
 def crear_usuario(user):
     user_dao = usuario_dao.Usuario_Dao()
     flag = user_dao.create(user)
+    return flag
+
+def obtener_usuario(email):
+    user_dao = usuario_dao.Usuario_Dao()
+    user = user_dao.get(email)
+    return user
+
+def validar_password(user, password):
+    print(user.get_password())
+    flag = servicioReglasNegocio.validar_password(user.get_password().encode('utf-8'), password)
     return flag
