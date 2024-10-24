dbb9e23377605ba60522902173df91ae25ddb206
Marcelo Portillo <mportillo79@gmail.com>
2024-10-24 13:47:09 -0300

    Se agrega historial de transacciones

--- controllers/controllerTransaccion.py
+++ controllers/controllerTransaccion.py
@@ -1,4 +1,15 @@
 from backend.clasesDAO import transaccion_dao
+
+
 def crear_transaccion(transaccion):
-   transaccionDao = transaccion_dao.Transaccion_Dao()
-   return transaccionDao.create(transaccion)
+    transaccionDao = transaccion_dao.Transaccion_Dao()
+    return transaccionDao.create(transaccion)
+
+
+def listar_transacciones(inversor):
+    transaccionDao = transaccion_dao.Transaccion_Dao()
+    transacciones_x_inversor = transaccionDao.get_by_fk(inversor)
+
+    for x in transacciones_x_inversor:
+        print(x)
+        print("\n")
