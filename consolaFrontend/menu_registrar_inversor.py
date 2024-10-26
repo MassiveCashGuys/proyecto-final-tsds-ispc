2842e0a50b5f97ab6c0420efe847fa4275f96022
Alan Marini <aland.marini@gmail.com>
2024-10-26 19:24:53 -0300

    separación de metodos confirmar datos desolicitar_datos_inversor en menu registrar inversor

--- consolaFrontend/menu_registrar_inversor.py
+++ consolaFrontend/menu_registrar_inversor.py
@@ -41,21 +41,28 @@ def solicitar_datos_inversor():
         print(f"Correo: {email}")
         print(f"Nombre: {nombre}")
         print(f"Apellido: {apellido}")
-        print(f"Contraseña: {pas}")  
+        print(f"Contraseña: {pas}")
+
+       
+        if confirmar_datos():
+            nuevo_usuario = usuario.Usuario(email, pasHas, 2)
+            nuevo_portafolio = controllerPortafolio.crear_portafolio()
+            nuevoInversor = inversor.Inversor(cuit, numero_documento, nombre, apellido, nuevo_portafolio, nuevo_tipo_inversor, nuevo_tipo_documento, nuevo_usuario)
+            return controllerRegistrarInversor.crear_inversor(nuevoInversor)
+        else:
+            print("\nIngrese los datos nuevamente.\n")
 
 
-        while True:
-            confirmacion = input("\n¿Son correctos? Por favor, ingrese 'S' para confirmar o 'N' para volver a ingresar los datos. ").strip().lower()
-            if confirmacion == 's':
-                nuevo_usuario = usuario.Usuario(email, pasHas, 2)
-                nuevo_portafolio = controllerPortafolio.crear_portafolio()
-                nuevoInversor = inversor.Inversor(cuit, numero_documento, nombre, apellido, nuevo_portafolio, nuevo_tipo_inversor, nuevo_tipo_documento, nuevo_usuario)
-                return controllerRegistrarInversor.crear_inversor(nuevoInversor)
-            elif confirmacion == 'n':
-                print("\nIngrese los datos nuevamente.\n")
-                break  
-            else:
-                print("Opción no válida. Por favor, ingrese 'S' para confirmar o 'N' para volver a ingresar los datos.")
+def confirmar_datos():
+    while True:
+        confirmacion = input("\n¿Son correctos? Por favor, ingrese 'S' para confirmar o 'N' para volver a ingresar los datos. ").strip().lower()
+        if confirmacion == 's':
+            return True
+        elif confirmacion == 'n':
+            return False
+        else:
+            print("Opción no válida. Por favor, ingrese 'S' para confirmar o 'N' para volver a ingresar los datos.")
+
 
 def reingrese_dato(funcion, variable, texto):
     while not funcion(variable):
