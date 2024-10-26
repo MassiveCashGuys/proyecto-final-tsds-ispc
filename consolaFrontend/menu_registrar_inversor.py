ec0642e19d5b468ef0bf2d00a85c3e71510805da
Alan Marini <aland.marini@gmail.com>
2024-10-26 00:54:37 -0300

    verificación de datos antes de guardar inversor

--- consolaFrontend/menu_registrar_inversor.py
+++ consolaFrontend/menu_registrar_inversor.py
@@ -6,45 +6,58 @@ import validadorDato.ValidacionDatos
 
 
 def solicitar_datos_inversor():
-    print(f"********Registro Inversor********")
+    while True:
+        print(f"*******Registro Inversor*******")
 
-    nuevo_tipo_inversor = controllerTipoInversor.cargar_menu_tipo_inversor()
-    nuevo_tipo_inversor = validadorDato.ValidacionDatos.tipo_de_dato(controllerTipoInversor.cargar_menu_tipo_inversor,nuevo_tipo_inversor)
-   
-    nuevo_tipo_documento = controllerTipoDocumento.cargar_menu_tipo_documento()
-    nuevo_tipo_documento = validadorDato.ValidacionDatos.tipo_de_dato(controllerTipoDocumento.cargar_menu_tipo_documento,nuevo_tipo_documento)
-   
-    cuit = input(f'Ingrese un CUIT: ')
-    cuit = reingrese_dato(validadorDato.ValidacionDatos.valida_existencia_cuit_en_db,cuit,"El cuit ya existe o el formato no es válido, ingreselo ")
-    
-    numero_documento = validadorDato.ValidacionDatos.extraccion_num_documento(cuit)
-    
-    email = input(f'Ingrese un correo: ')
-    email = reingrese_dato(validadorDato.ValidacionDatos.valida_existencia_mail_bd,email,"El correo ya existe o el formato no es válido, ingreselo ")
-   
-    nombre = input(f'Ingrese su nombre: ')
-   
-    apellido = input(f'Ingrese su apellido: ')
+        nuevo_tipo_inversor = controllerTipoInversor.cargar_menu_tipo_inversor()
+        nuevo_tipo_inversor = validadorDato.ValidacionDatos.tipo_de_dato(controllerTipoInversor.cargar_menu_tipo_inversor, nuevo_tipo_inversor)
+ 
+        nuevo_tipo_documento = controllerTipoDocumento.cargar_menu_tipo_documento()
+        nuevo_tipo_documento = validadorDato.ValidacionDatos.tipo_de_dato(controllerTipoDocumento.cargar_menu_tipo_documento, nuevo_tipo_documento)
+       
+        cuit = input('Ingrese un CUIT: ')
+        cuit = reingrese_dato(validadorDato.ValidacionDatos.valida_existencia_cuit_en_db, cuit, "El CUIT ya existe o el formato no es válido, ingréselo")
+        
+        numero_documento = validadorDato.ValidacionDatos.extraccion_num_documento(cuit)
+        
+        email = input('Ingrese un correo: ')
+        email = reingrese_dato(validadorDato.ValidacionDatos.valida_existencia_mail_bd, email, "El correo ya existe o el formato no es válido, ingréselo")
+       
+        nombre = input('Ingrese su nombre: ')
+       
+        apellido = input('Ingrese su apellido: ')
 
-    print(f'Ingrese una contraseña entre 8-16 digitos que contenga algún caracter especial y números.')
-    pas = input(f'Contraseña: ')
-    #confirmación de datos son correctos
-    pas= reingrese_dato(validadorDato.ValidacionDatos.pw_es_valido,pas,"Ingrese una contraseña")
-    pasHas= servicioReglasNegocio.crear_encriptacion_password(pas)
+        print('Ingrese una contraseña entre 8-16 dígitos que contenga algún carácter especial y números.')
+        pas = input('Contraseña: ')
+        
+        pas = reingrese_dato(validadorDato.ValidacionDatos.pw_es_valido, pas, "Ingrese una contraseña")
+        pasHas = servicioReglasNegocio.crear_encriptacion_password(pas)
 
-    nuevo_usuario = usuario.Usuario(email, pasHas, 2)
-    nuevo_portafolio = controllerPortafolio.crear_portafolio()
-    nuevoInversor = inversor.Inversor(cuit, numero_documento, nombre, apellido, nuevo_portafolio, nuevo_tipo_inversor, nuevo_tipo_documento, nuevo_usuario)
-    
-    return controllerRegistrarInversor.crear_inversor(nuevoInversor)
-    
+        print("\n*******Verifique los datos ingresados*******")
+
+        print(f"Tipo de Inversor: {nuevo_tipo_inversor}")
+        print(f"Tipo de Documento: {nuevo_tipo_documento}")
+        print(f"CUIT: {cuit}")
+        print(f"Correo: {email}")
+        print(f"Nombre: {nombre}")
+        print(f"Apellido: {apellido}")
+        print(f"Contraseña: {pas}")  
+
+
+        while True:
+            confirmacion = input("\n¿Son correctos? Por favor, ingrese 'S' para confirmar o 'N' para volver a ingresar los datos. ").strip().lower()
+            if confirmacion == 's':
+                nuevo_usuario = usuario.Usuario(email, pasHas, 2)
+                nuevo_portafolio = controllerPortafolio.crear_portafolio()
+                nuevoInversor = inversor.Inversor(cuit, numero_documento, nombre, apellido, nuevo_portafolio, nuevo_tipo_inversor, nuevo_tipo_documento, nuevo_usuario)
+                return controllerRegistrarInversor.crear_inversor(nuevoInversor)
+            elif confirmacion == 'n':
+                print("\nIngrese los datos nuevamente.\n")
+                break  
+            else:
+                print("Opción no válida. Por favor, ingrese 'S' para confirmar o 'N' para volver a ingresar los datos.")
 
 def reingrese_dato(funcion, variable, texto):
-
-    while True:
-        if funcion(variable):
-            return variable
-        else:
-            variable = input(f"{texto} de nuevo: ")
-    
-
+    while not funcion(variable):
+        variable = input(f"{texto} de nuevo: ")
+    return variable
