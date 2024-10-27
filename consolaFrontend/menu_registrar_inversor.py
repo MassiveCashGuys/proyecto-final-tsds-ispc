f7aced6c3b1e7084eb1919a457ebd11e33624286
Julio Martin <faria@DESKTOP-TLUUL68>
2024-10-27 16:16:30 -0300

    Mejorando compra de acciones y registrar inversor

--- consolaFrontend/menu_registrar_inversor.py
+++ consolaFrontend/menu_registrar_inversor.py
@@ -1,6 +1,6 @@
-from controllers import controllerTipoDocumento, controllerPortafolio,controllerTipoInversor, controllerRegistrarInversor, controllerInicioSesion, controllerUsuario
-from consolaFrontend import menu_tipo_documento, menu_tipos
-from negocio import servicioReglasNegocio, usuario, inversor, tipoInversor
+from controllers import controllerTipoDocumento, controllerPortafolio,controllerTipoInversor, controllerRegistrarInversor
+from consolaFrontend import menu_tipo_documento
+from negocio import servicioReglasNegocio, usuario, inversor 
 import validadorDato.ValidacionDatos
 
 
@@ -8,35 +8,27 @@ import validadorDato.ValidacionDatos
 def solicitar_datos_inversor():
     print(f"********Registro Inversor********")
 
-    nuevo_tipo_inversor = controllerTipoInversor.cargar_menu_tipo_inversor()
-    nuevo_tipo_inversor = validadorDato.ValidacionDatos.tipo_de_dato(controllerTipoInversor.cargar_menu_tipo_inversor,nuevo_tipo_inversor)
+    nuevo_tipo_inversor = menu_tipo_inversor()
    
-    nuevo_tipo_documento = controllerTipoDocumento.cargar_menu_tipo_documento()
-    nuevo_tipo_documento = validadorDato.ValidacionDatos.tipo_de_dato(controllerTipoDocumento.cargar_menu_tipo_documento,nuevo_tipo_documento)
+    nuevo_tipo_documento = menu_tipo_documento()
    
-    cuit = input(f'Ingrese un CUIT: ')
-    cuit = reingrese_dato(validadorDato.ValidacionDatos.valida_existencia_cuit_en_db,cuit,"El cuit ya existe o el formato no es válido, ingreselo ")
+    cuit = menu_cuit()
     
     numero_documento = validadorDato.ValidacionDatos.extraccion_num_documento(cuit)
     
-    email = input(f'Ingrese un correo: ')
-    email = reingrese_dato(validadorDato.ValidacionDatos.valida_existencia_mail_bd,email,"El correo ya existe o el formato no es válido, ingreselo ")
+    email = menu_email()
    
     nombre = input(f'Ingrese su nombre: ')
    
     apellido = input(f'Ingrese su apellido: ')
 
-    print(f'Ingrese una contraseña entre 8-16 digitos que contenga algún caracter especial y números.')
-    pas = input(f'Contraseña: ')
-    #confirmación de datos son correctos
-    pas= reingrese_dato(validadorDato.ValidacionDatos.pw_es_valido,pas,"Ingrese una contraseña")
-    pasHas= servicioReglasNegocio.crear_encriptacion_password(pas)
-
-    nuevo_usuario = usuario.Usuario(email, pasHas, 2)
-    nuevo_portafolio = controllerPortafolio.crear_portafolio()
-    nuevoInversor = inversor.Inversor(cuit, numero_documento, nombre, apellido, nuevo_portafolio, nuevo_tipo_inversor, nuevo_tipo_documento, nuevo_usuario)
+    pasHas= menu_contraseña()
     
-    return controllerRegistrarInversor.crear_inversor(nuevoInversor)
+    nuevo_portafolio = controllerPortafolio.crear_objeto_portafolio()
+    nuevo_usuario = usuario.Usuario(email, pasHas, 2)
+    inversorFianl = inversor.Inversor(cuit, numero_documento, nombre, apellido, nuevo_portafolio, nuevo_tipo_inversor, nuevo_tipo_documento, nuevo_usuario)
+   
+    return controllerRegistrarInversor.crear_inversor(inversorFianl)
     
 
 def reingrese_dato(funcion, variable, texto):
@@ -46,5 +38,34 @@ def reingrese_dato(funcion, variable, texto):
             return variable
         else:
             variable = input(f"{texto} de nuevo: ")
-    
+            
 
+    
+def menu_cuit():
+    cuit = input(f'Ingrese un CUIT: ')
+    cuit = reingrese_dato(validadorDato.ValidacionDatos.valida_existencia_cuit_en_db,cuit,"El cuit ya existe o el formato no es válido, ingreselo ")
+    return cuit
+
+def menu_email():
+    email = input(f'Ingrese un correo: ')
+    email = reingrese_dato(validadorDato.ValidacionDatos.valida_existencia_mail_bd,email,"El correo ya existe o el formato no es válido, ingreselo ")
+    return email
+
+def menu_contraseña():
+    print(f'Ingrese una contraseña entre 8-16 digitos que contenga algún caracter especial y números.')
+    pas = input(f'Contraseña: ')
+    pas= reingrese_dato(validadorDato.ValidacionDatos.pw_es_valido,pas,"Ingrese una contraseña")
+    return servicioReglasNegocio.crear_encriptacion_password(pas)
+
+def menu_tipo_inversor():
+    nuevo_tipo_inversor = controllerTipoInversor.cargar_menu_tipo_inversor()
+    nuevo_tipo_inversor = validadorDato.ValidacionDatos.tipo_de_dato(controllerTipoInversor.cargar_menu_tipo_inversor,nuevo_tipo_inversor)
+    return nuevo_tipo_inversor
+
+def menu_tipo_documento():
+    nuevo_tipo_documento = controllerTipoDocumento.cargar_menu_tipo_documento()
+    nuevo_tipo_documento = validadorDato.ValidacionDatos.tipo_de_dato(controllerTipoDocumento.cargar_menu_tipo_documento,nuevo_tipo_documento)
+    return nuevo_tipo_documento
+
+
+    
--- controllers/controllerRegistrarInversor.py
+++ controllers/controllerRegistrarInversor.py
@@ -1,6 +1,6 @@
 from consolaFrontend import menu_tipo_documento
 from consolaFrontend import menu_registrar_inversor
-from backend.clasesDAO import usuario_dao, inversor_dao
+from backend.clasesDAO import inversor_dao
 from controllers import controllerPortafolio
 
 def cargar_menu_tipo_documento():
@@ -11,15 +11,12 @@ def cargar_menu_registro_inversor():
 
 
 def crear_inversor(inversor):
-    
-    userDao = usuario_dao.Usuario_Dao();
-    userDao.create(inversor.get_user())
     inversorDao = inversor_dao.InversorDao()
     updateInversor= inversorDao.create(inversor)
     updatePortafolio = controllerPortafolio.obteder_portafolio(updateInversor.get_portafolio())
     updateInversor.set_portafolio(updatePortafolio)
     cargar_saldo_inicial(updateInversor)
-    return updateInversor
+    return updateInversor 
 
 def cargar_saldo_inicial(inversor):
     controllerPortafolio.cargar_saldo_inicial(inversor.get_portafolio().get_id_portafolio())
